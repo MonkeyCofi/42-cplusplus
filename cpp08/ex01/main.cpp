37199603f470ae5f4036ea9c0f0c4ad9dcf5ad85
MonkeyCofi <piercediet06@gmail.com>
2025-02-10 09:52:24 +0400

    idk

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pipolint <pipolint@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:37:27 by pipolint          #+#    #+#             */
-/*   Updated: 2025/02/09 17:45:56 by pipolint         ###   ########.fr       */
+/*   Updated: 2025/02/10 09:46:48 by pipolint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ int main(void)
 	Span s(5);
 	try
 	{
-		s.addNumber(1);
-		s.addNumber(2);
-		s.addNumber(3);
+		
+		//s.addNumber(1);
+		//s.addNumber(2);
+		//s.addNumber(3);
 	}
 	catch (Span::VectorFullException& ex)
 	{
